48f3cf96f094c527fec77a0e98dca466da5abab2
Vlad <vladislav.lobanov@outlook.com>
2021-10-23 01:04:58 +0200

    increase and decrease buttons css added

--- src/components/Prices.vue
+++ src/components/Prices.vue
@@ -1,13 +1,63 @@
 <template>
     <div>
-        <div>
-            <button @click="increase(activeEl)">
-                +
-            </button>
-            <button @click="decrease(activeEl)">
-                –
-            </button>
+        <div
+            class="decreaseIncreaseButtonContainer"
+            @click="increase(activeEl)"
+        >   
+            <div
+                class="decreaseIncreaseButton"
+            >
+                <svg
+                    class="increaseDescreaseSvg"
+                    width="14"
+                    height="14"
+                    viewBox="0 0 14 14"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <line
+                        y1="7"
+                        x2="14"
+                        y2="7"
+                        stroke="black"
+                        stroke-width="2"
+                    />
+                    <line
+                        x1="7"
+                        y1="14"
+                        x2="7"
+                        stroke="black"
+                        stroke-width="2"
+                    />
+                </svg>
+            </div>
         </div>
+        <div
+            class="decreaseIncreaseButtonContainer"
+            @click="decrease(activeEl)"
+        >   
+            <div
+                class="decreaseIncreaseButton"
+            >
+                <svg
+                    class="increaseDescreaseSvg"
+                    width="14"
+                    height="2"
+                    viewBox="0 0 14 2"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <line
+                        y1="1"
+                        x2="14"
+                        y2="1"
+                        stroke="black"
+                        stroke-width="2"
+                    />
+                </svg>
+            </div>
+        </div>
+    
         <p>
             Number of tickets available:
             {{ showNumberOfTickets(activeEl) }}
@@ -88,4 +138,41 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+
+.decreaseIncreaseButtonContainer {
+    height: 40.5px;
+    width: 54px;
+    border-radius: 20.25px;
+    position:relative;
+    overflow: hidden;
+    cursor: pointer;
+}
+
+ .decreaseIncreaseButtonContainer::before {
+    content: "";
+    position: absolute;
+    border: 5px solid white;
+    box-sizing: border-box;
+    width: 58.6px;
+    bottom: 5.5px;
+    transform: rotate(-25.5deg) translate(0px, -100%);
+}  
+
+.decreaseIncreaseButton {
+    height: 100%;
+    width: 100%;
+    background: #ffffff;
+    border: 2px solid #55c57a;
+    box-sizing: border-box;
+    border-radius: 20.25px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.increaseDescreaseSvg {
+    position: absolute;
+}
+
+</style>
